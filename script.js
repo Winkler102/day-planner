92651f6cdd00048f0d30972a36261a560f0c015b
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-04-14 13:47:46 -0500

    added date

---
+++ script.js
@@ -0,0 +1,35 @@
+currentDate = moment().format("dddd, MMMM Do YYYY")
+$("#currentDay").text(currentDate);
+
+$("#textDiv").on("click", function () {
+    var text = $(".text9am")
+        .text()
+        .trim();
+
+    var textInput = $("<textarea>")
+        .addClass("changeText border-0")
+        .val(text);
+
+    $(".text9am").replaceWith(textInput);
+})
+
+$("#btn9am").on("click", function () {
+
+    var text = $(".changeText")
+        .val();
+
+    var textInput = $("<p>")
+        .addClass("text9am")
+        .text(text);
+
+    $(".changeText").replaceWith(textInput);
+
+    localStorage.setItem("time-9am", textInput.text())
+});
+
+function load() {
+    var stored = localStorage.getItem("time-9am");
+    $(".text9am").text(stored);
+}
+
+load();
