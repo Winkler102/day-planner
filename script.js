6c0ae7d33230bd9f11298cf49d1cf344d511e98f
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-04-14 20:38:05 -0500

    some touch ups

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ currentDate = moment().format("dddd, MMMM Do YYYY")
 $("#currentDay").text(currentDate);
 
 $("#textDiv9am").on("click", function () {
+
     var text = $(".text9am")
         .text()
         .trim();
 
     var textInput = $("<textarea>")
-        .addClass("changeText border-0")
+        .addClass("changeText")
         .val(text);
 
     $(".text9am").replaceWith(textInput);
@@ -24,12 +25,14 @@ $("#btn9am").on("click", function () {
 
     $(".changeText").replaceWith(textInput);
 
+    $("#textDiv9am").html($("#textDiv9am").html().trim())
     localStorage.setItem("time-9am", textInput.text())
 });
 
 function load() {
     var stored = localStorage.getItem("time-9am");
-    $(".text9am").text(stored);
+    $(".text9am").text(stored.trim());
+    $("#textDiv9am").html($("#textDiv9am").html().trim())
 }
 
 load();
